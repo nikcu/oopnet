cc35ad361a1e85c97c01ab4b4c9433edc11a9a9c
NikolaC <mail@nikolac.com>
2025-08-29 20:08:28 +0200

    Add Icon

--- WindowsForms/SettingsForm.Designer.cs
+++ WindowsForms/SettingsForm.Designer.cs
@@ -28,10 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SettingsForm));
+            SuspendLayout();
+            // 
+            // SettingsForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(838, 441);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            MinimumSize = new Size(480, 320);
+            Name = "SettingsForm";
+            Text = "Form1";
+            ResumeLayout(false);
         }
 
         #endregion
